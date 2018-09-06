8f06a891f2c88cf21275328188b6e10715da25f1
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2018-09-06 12:32:02 +0300

    creating script function added
    creating script function added

--- file_system.js
+++ file_system.js
@@ -3,4 +3,58 @@
  */
 
 
-const path = process.argv[2];
+const url = process.argv[2];
+const fs = require("fs");
+const path = require('path');
+
+const scriptString = `function filewalker(dir, done) {
+const fs = require("fs");
+const path = require('path');
+let results = [];
+
+fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+
+    var pending = list.length;
+
+    if (!pending) return done(null, results);
+
+    list.forEach(function(file){
+        file = path.resolve(dir, file);
+
+        fs.stat(file, function(err, stat){
+            // If directory, execute a recursive call
+            if (stat && stat.isDirectory()) {
+                // Add directory to array [comment if you need to remove the directories from the array]
+                //results.push(file);
+                results.push(path.relative("${url}", file));
+
+                filewalker(file, function(err, res){
+                    results = results.concat(res);
+                    if (!--pending) done(null, results);
+                });
+            } else {
+                //results.push(file);
+                results.push(path.relative("${url}", file));
+
+                if (!--pending) done(null, results);
+            }
+        });
+    });
+});
+};
+
+
+filewalker("${url}", function(err, data){
+    if(err){
+        throw err;
+    }
+
+    console.log(data);
+});`;
+
+fs.writeFile(url + "/summary.js", scriptString, function(error){
+ if(error) throw error;
+});
+
+
