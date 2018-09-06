8ba404fb6948887e4b645f1e1ebae3896e317718
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2018-09-06 20:21:48 +0300

    final commit
    final commit

--- file_system.js
+++ file_system.js
@@ -3,8 +3,19 @@
  */
 
 const url = process.argv[2];
+
+if (url == undefined){
+    return;
+}
+
 const fs = require("fs");
 const path = require('path');
+
+fs.exists(url, function (err) {
+    if (err){
+        return;
+    }
+});
 
 const scriptString = `function filewalker(dir, done) {
 const fs = require("fs");
@@ -46,9 +57,87 @@ filewalker("${url}", function(err, data){
     console.log(data);
 });`;
 
+// 01
+
 fs.writeFile(url + "/summary.js", scriptString, function(error){
  if(error) throw error;
 });
+
+// 02
+var copyright = require("./config.json");
+var newFolderPath = url + "/" + url.split("/").pop();
+
+function makeDir(path){
+    fs.mkdir(path, function(error){
+        if(error) throw error;
+    });
+}
+
+function fillTxtList(nPath, fn){
+    fs.readdir(nPath, function(err, fileNames){
+        if (err)
+            return done(err);
+
+        var list = [];
+
+        for(let i = 0; i < fileNames.length; i++) {
+            let fileName = fileNames[i];
+
+            if(path.extname(fileName) === ".txt") {
+                list.push(nPath + "/" + fileName);
+            }
+        }
+
+        fn(list);
+    });
+}
+
+function addCopyright(str, fUrl) {
+    fs.readFile(fUrl, 'utf8', function(err, contents) {
+        if (err) {
+            return console.log(err);
+        }
+
+        let text = str + contents + str;
+
+        fs.writeFile(fUrl, text, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+        });
+    });
+}
+
+fs.exists(newFolderPath, function (error){
+    if (error){
+        throw error;
+    }
+
+    makeDir(newFolderPath);
+});
+
+
+
+fillTxtList(url, function (list) {
+    var files = list;
+
+    files.forEach(function (item) {
+        fs.copyFile(item, newFolderPath + "/" + item.split("/").pop(), function(error){
+            if (error)
+                console.log(error);
+
+            addCopyright(copyright["copyright"], newFolderPath + "/" + item.split("/").pop());
+
+            fs.watchFile(newFolderPath + "/" + item.split("/").pop(), function(curr, prev){
+                console.log(newFolderPath + "/" + item.split("/").pop() + " is EDIT!");
+            });
+        });
+    });
+});
+
+
 
 
 
